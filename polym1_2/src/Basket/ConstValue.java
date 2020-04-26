9642f64b36806c02d1c569b1e192b2ed85fc4d28
gitH_fujihir <rokokoi20121230@gmail.com>
2020-04-26 22:23:47 +0900

    古いファイルの削除

--- polym1_2/src/Basket/ConstValue.java
+++
@@ -1,7 +0,0 @@
-package Basket;
-
-public class ConstValue {
-    public static final String STORE_NM_KIRITAN = "　　　　きりたんストア";
-    public static final String STORE_NM_YUKARI = "　　　　　ゆかりショップ";
-    public static final String STORE_NM_MAKI = "　　　　　マキマキマーケット";
-}
